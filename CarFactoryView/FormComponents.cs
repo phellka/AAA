4649f41513b1b727bc6304baca29d8ec4f770f26
ktrepfk <ktrepfk@mail.ru>
2022-02-01 20:15:56 +0400

    half

---
+++ CarFactoryView/FormComponents.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CarFactoryContracts.BindingModels;
+using CarFactoryContracts.BusinessLogicsContracts;
+using Unity;
+
+namespace CarFactoryView
+{
+    public partial class FormComponents : Form
+    {
+        private readonly IComponentLogic logic;
+        public FormComponents(IComponentLogic logic)
+        {
+            InitializeComponent();
+            this.logic = logic;
+        }
+
+        private void FormComponents_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadData()
+        {
+            try
+            {
+                var list = logic.Read(null);
+                if (list != null)
+                {
+                    dataGridView.DataSource = list;
+                    dataGridView.Columns[0].Visible = false;
+                    dataGridView.Columns[1].AutoSizeMode =
+                        DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+    }
+}
